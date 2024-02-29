54e5346a31d16d61a840e38115b54aad4e8dc914
fujivara <marat.skyba@gmail.com>
2024-02-29 15:20:43 +0200

    bug: Fix environment for production

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   WEATHER_API_KEY: process.env['WEATHER_API_KEY'],
   WEATHER_API_BASE_URL: process.env['WEATHER_API_BASE_URL'],
-  API_BASE_URL: process.env['API_BASE_URL'],
+  API_BASE_URL: 'http://localhost:4455',
 };
