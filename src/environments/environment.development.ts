f43b7e1345d93cd43f71b786fc0c3d7e3289efc8
fujivara <marat.skyba@gmail.com>
2024-02-29 12:38:40 +0200

    Revert "refactoring: Styles optimization"
    This reverts commit 78d8616c8a1cd60f7b38150b20b8a936c227b9c6.

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  production: false,
+  production: true,
   WEATHER_API_KEY: process.env['WEATHER_API_KEY'],
   WEATHER_API_BASE_URL: process.env['WEATHER_API_BASE_URL'],
-  API_BASE_URL: 'http://localhost:4455',
+  API_BASE_URL: process.env['API_BASE_URL'],
 };
